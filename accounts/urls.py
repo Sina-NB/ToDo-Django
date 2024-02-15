b038b33c5cc862bd4c1d270e66406a988203e640
Sina Nabigol <sina.nabigol77@gmail.com>
2024-02-15 13:27:01 +0330

    Fix bugs of login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
-app_name = 'accounts'
+app_name = "accounts"
 urlpatterns = [
-    path('login', views.LoginView.as_view(), name='login-view'),
-    path('logout', views.LogoutView.as_view(), name='logout-view'),
-    path('register', views.RegisterView.as_view(), name='register-view'),
+    path("login/", views.LoginView.as_view(), name="login-view"),
+    path("logout/", views.LogoutView.as_view(), name="logout-view"),
+    path("register/", views.RegisterView.as_view(), name="register-view"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,53 +1,61 @@
 from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.views import View
+from .forms import CustomAuthenticationForm
+
 
 # Create your views here.
 class LoginView(View):
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            redirect('/')
+            return redirect("/")
         else:
             form = AuthenticationForm()
-            context = {'form': form}
-            return render(request, 'accounts/login.html', context)
-    
+            context = {"form": form}
+            return render(request, "accounts/login.html", context)
+
     def post(self, request, *args, **kwargs):
-        form = AuthenticationForm(request, data=request.POST)
+        form = CustomAuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            remember_me = form.cleaned_data.get("remember_me")
             user = authenticate(request, username=username, password=password)
             if user is not None:
+                if remember_me:
+                    request.session.set_expiry(1209600)
+                else:
+                    request.session.set_expiry(0)
                 login(request, user)
-                return redirect('/')
-        return render(request, 'accounts/login.html', {'form': form})
+                return redirect("/")
+        return render(request, "accounts/login.html", {"form": form})
+
 
 class RegisterView(View):
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            redirect('/')
+            return redirect("/")
         else:
             form = UserCreationForm()
-            context = {'form': form}
-            return render(request, 'accounts/register.html', context)
-    
+            context = {"form": form}
+            return render(request, "accounts/register.html", context)
+
     def post(self, request, *args, **kwargs):
         form = UserCreationForm(data=request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
             form.save()
-            print('Hello world!!')
-            return redirect('/accounts/login')
-        return render(request, 'accounts/register.html', {'form': form})
+            return redirect("/accounts/login")
+        return render(request, "accounts/register.html", {"form": form})
+
 
 class LogoutView(View):
 
+    @method_decorator(login_required)
     def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            logout(request)
-        return redirect('/accounts/login')
+        logout(request)
+        return redirect("/accounts/login")
