ace624aba77bcff08bad48ebd48725c25f4c6248
Sina Nabigol <sina.nabigol77@gmail.com>
2024-02-25 18:34:40 +0330

    Reformat code

--- ToDo_Django/accounts/tests/test_login_view.py
+++ ToDo_Django/accounts/tests/test_login_view.py
@@ -3,20 +3,24 @@ from django.urls import reverse
 import pytest
 from django.contrib.auth.models import User
 
+
 @pytest.fixture
 def client():
     client = Client()
     return client
 
+
 @pytest.fixture
 def user():
-    user = User.objects.create_superuser(username='test_user', password='AdfgS5784@')
+    user = User.objects.create_superuser(username="test_user", password="AdfgS5784@")
     return user
+
 
 @pytest.fixture
 def url():
-    url = reverse('accounts:login-view')
+    url = reverse("accounts:login-view")
     return url
+
 
 @pytest.mark.django_db
 class TestLoginView:
